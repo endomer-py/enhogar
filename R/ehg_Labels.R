42c894c7ffae638735b365a94e8c70ac7fcf292b
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-02-19 12:20:20 -0400

    feat: labels api

---
+++ R/ehg_Labels.R
@@ -0,0 +1,55 @@
+#' Asigna etiquetas de datos a las variables especificadas
+#'
+#' `r lifecycle::badge("experimental")`
+#'
+#' @param tbl [data.frame]: Conexión a base de datos o dataframe con los datos
+#' @param vars [character]: Si especificado, solo se asignaran las etiquetas a esas variables.
+#'
+#' @return Los datos introducidos en el argumento \code{tbl} pero con etiquetas de datos
+#'
+#' @details
+#'   Esta función permite asignar etiquetas de datos a las variables de un data.frame
+#'   o equivalente, a partir del diccionario de la encuesta.
+#'
+#' @seealso
+#'   Etiquetas de datos \code{vignette("labels", package = "endomer")}
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   ehg18 <- data.frame(ZONA = c(1, 2))
+#'   str(ehg18)
+#'   str(ftc_setLabels(ehg18))
+#'}
+ehg_setLabels <- function(tbl, vars = NULL) {
+  endomer::setLabels(tbl, vars, enhogar::dict)
+}
+
+
+
+#' Utiliza las etiquetas de datos de una variable
+#'
+#' `r lifecycle::badge("experimental")`
+#'
+#' @param tbl [data.frame]: Conexión a base de datos o dataframe con los datos
+#' @param vars [character]: Si especificado, solo se asignaran las etiquetas a esas variables.
+#'
+#' @return Los datos suministrados en el argumento \code{tbl}, pero en lugar de
+#'   valores utilizando las etiquetas de datos correspondientes
+#'
+#' @seealso
+#'   Etiquetas de datos \code{vignette("labels", package = "endomer")}
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'   ehg18 <- data.frame(ZONA = c(1, 2))
+#'   ehg18
+#'   ftc_useLabels(ehg18)
+#'}
+ehg_useLabels <- function(tbl, vars = NULL) {
+  endomer::useLabels(tbl, vars, enhogar::dict)
+}
+
